620b3589c0cc7623d93f9fae8febaa3d33f85d17
David Revay <daverevay@gmail.com>
2026-01-06 08:50:48 +1100

    chore: lint fixes

--- packages/main/src/trpc/procedures.ts
+++ packages/main/src/trpc/procedures.ts
@@ -8,7 +8,7 @@ export const middleware = t.middleware;
 export const publicProcedure = t.procedure;
 
 // Middleware to resolve services from TSyringe container
-export const injectService = <T>(serviceToken: any) => {
+export const injectService = <T>(serviceToken: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
   return middleware(async ({ next, ctx }) => {
     const service = container.resolve<T>(serviceToken);
     return next({
